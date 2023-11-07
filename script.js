3d1595ca274b8d16ce533d2cd8b9e44be3b23c16
rusosso <88565208+rusosso@users.noreply.github.com>
2023-11-07 22:20:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function intronext()
 {
-	document.getElementById("body").style.transform="translateY(-100vh)";
-	document.getElementById("introgrid").style.opacity="0";
-	setTimeout(introcontentloose,1000);
+	
+	document.getElementById("introcontaineroverlay").style.opacity="1";
+	document.getElementById("griddivlettersoverlay").style.borderColor="white";
+	document.getElementById("div8").style.opacity="0";
+	setTimeout(introcontentloose,500);
 }
 
 function introcontentloose()
 {
-	document.getElementById("introcontainer").innerHTML=" ";
+	document.getElementById("div8").style.transform="translateY(-100vh)";
 	document.getElementById("introcontainer").style.animation="none";
+	document.getElementById("introgrid").style.transform="scale(60%)";
+	document.getElementById("body").style.overflowY="auto";
+	document.getElementById("introarrow").style.opacity="1";
 }
