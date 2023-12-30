c3f41b6811a22431a8931163d48ee74fab007a56
rusosso <88565208+rusosso@users.noreply.github.com>
2023-12-30 19:37:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+window.onbeforeunload = function load()
+{
+	window.scrollTo(0, 0);
+}
+
 function intronext()
 {
 	
 	document.getElementById("introcontaineroverlay").style.opacity="1";
 	document.getElementById("griddivlettersoverlay").style.borderColor="white";
 	document.getElementById("div8").style.opacity="0";
+	document.getElementById("introgrid").style.transform="scale(60%)";
 	setTimeout(introcontentloose,500);
 }
 
 function introcontentloose()
 {
 	document.getElementById("div8").style.transform="translateY(-100vh)";
+	document.getElementById("griddivlettersoverlay").style.background="rgba(0,0,0,0.5)";
 	document.getElementById("introcontainer").style.animation="none";
-	document.getElementById("introgrid").style.transform="scale(60%)";
+	document.getElementById("introgrid").style.transform="scale(60%) translateY(-10vh)";
+	document.getElementById("introgrid").style.position="fixed";
+	document.getElementById("introarrow").style.opacity="1";
 	document.getElementById("body").style.overflowY="auto";
 	document.getElementById("introarrow").style.opacity="1";
+	setTimeout(introarrow_animation,500);
 }
+
+function introarrow_animation()
+{
+	document.getElementById("introarrow").style.animation="introarrow 3s infinite";
+	setTimeout(scroll,50);
+}
+
